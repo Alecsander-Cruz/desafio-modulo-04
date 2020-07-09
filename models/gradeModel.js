58e71da38f11a288f58b31b658bd88ab94ad8ec1
Alecsander Cruz <alecsandercruz93@gmail.com>
2020-07-08 22:09:34 -0300

    ajuste no gradeModel.js

--- models/gradeModel.js
+++ models/gradeModel.js
@@ -23,6 +23,12 @@ export default (mongoose) => {
     },
   });
 
+  schema.method('toJSON', function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+  });
+
   const grade = mongoose.model('grades-test', schema, 'grades-test');
   return grade;
 };
